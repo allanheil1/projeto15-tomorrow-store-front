07ff4a17e3740657704a97efdd1da5e25e66b15e
allanheil1 <allanheil1@gmail.com>
2023-01-24 17:49:45 -0300

    Project Structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import UserContext from './contexts/UserContext';
 
-function App() {
+export default function App() {
+
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <UserContext.Provider value={{ token, setToken, user, setUser }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<SignInPage />}/>
+            <Route path='/sign-up' element={<SignUpPage />}/>
+            <Route path='/home' element={<HomePage />}/>
+            <Route path='/cart' element={<CartPage />}/>
+            <Route path='/orders' element={<OrderPage />}/>
+          </Routes>
+        </BrowserRouter>
+      </UserContext.Provider>
+    </>
   );
 }
 
-export default App;
+
