be32690a0a6fff9342b9bc7218b85cc37cad0292
allanheil1 <allanheil1@gmail.com>
2023-01-25 17:48:57 -0300

    style: SignIn Layout & functions (No logo/color)

--- src/pages/SignInPage/SignInPage.js
+++ src/pages/SignInPage/SignInPage.js
@@ -1,15 +1,74 @@
-import { SignInPageStyle } from './style';
+import { Link, useNavigate } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import axios from 'axios';
+
+import Logo from '../../assets/logo.jpeg';
+import { SignInPageStyle, Cadastre, Form } from './style';
+import UserContext from '../../contexts/UserContext';
 
 export default function SignInPage() {
 
-    return(
-        <>
-            <SignInPageStyle>
-                <h1>SignInPage</h1>
-            </SignInPageStyle>
-        </>
+    const navigate = useNavigate();
+    const { setToken } = useContext(UserContext);
+    const { setUserPhoto } = useContext(UserContext);
+    const [isLoading, setIsLoading] = useState(false);
+    const [loginData, setLoginData] = useState({
+      email: '',
+      password: ''
+    });
 
-    )
+    function LoginRequest(e){
+        e.preventDefault();
+        setIsLoading(true);
+        const promise = axios.post('LOGIN URL BACKEND', loginData);
+        promise.then((res) => {
+          setIsLoading(false);
+
+          //SET VARIABLES FROM SERVER RESPONSE
+          //setToken(res.data.token);
+          //setUserPhoto(res.data.image);
+
+          navigate('/home');
+        });
+        promise.catch((err) => {
+          setIsLoading(false);
+          const errorMsg = err.response.statusText;
+          alert(`Erro: ${errorMsg}`);
+        })
+    }
+
+    function OnChange(e) {
+        setLoginData({ ...loginData, [e.target.name]: e.target.value });
+    }
+
+    return (
+
+        <SignInPageStyle>
+          <img src={Logo}/>
+  
+          <Form onSubmit={LoginRequest}>
+            <input 
+              type='email' placeholder='email'
+              value={loginData.email} name='email'
+              onChange={OnChange} required disabled={isLoading}
+            />
+            <input 
+              type='password' placeholder='senha'
+              value={loginData.password} name='password'
+              onChange={OnChange} required disabled={isLoading}
+            />
+            <button type='submit' disabled={isLoading}>
+                Login
+            </button>
+          </Form>
+  
+          <Link to={isLoading ? '' : '/sign-up'}>
+            <Cadastre>
+              Ainda sem conta? Faça seu cadastro!
+            </Cadastre>
+          </Link>
+        </SignInPageStyle>
+    );
 
 }
 
--- src/pages/SignInPage/style.js
+++ src/pages/SignInPage/style.js
@@ -1,5 +1,69 @@
 import styled from 'styled-components';
 
 export const SignInPageStyle = styled.div`
+  width: 100%;
+  height: 100vh;
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  img{
+    width: 300px;
+    height: 300px;
+    margin-bottom: 32px;
+  }
 
+  input{
+    font-family: 'Orbitron', sans-serif;
+    height: 45px;
+    width: 303px;
+    margin-bottom: 6px;
+    background: #FFFFFF;
+    border: 1px solid #D5D5D5;
+    border-radius: 5px;
+    font-size: 20px;
+    color: #666666;
+    padding-left: 10px;
+    ::placeholder{
+      color: #DBDBDB;
+      font-size: 20px;
+      text-decoration: none;
+    }
+  }
+
+  button{
+    font-family: 'Orbitron', sans-serif;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 45px;
+    width: 303px;
+    border: none;
+    border-radius: 5px;
+    font-size: 20px;
+    color: white;
+    text-align: center;
+    margin-bottom: 25px;
+    background-color: #B1B1B1;
+    cursor: pointer;
+  }
+  button:disabled{
+      opacity: 0.75;
+  }
+`
+
+export const Cadastre = styled.div`
+  font-family: 'Orbitron', sans-serif;
+  color: #B1B1B1;
+  font-size: 15px;
+  text-decoration: underline;
+`
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 30px 0 25px;
 `
