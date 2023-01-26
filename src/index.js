147f36fbceae7d8dd42c042f34fbb8781b90242b
encarnacao <cencarnacaoq@gmail.com>
2023-01-26 16:59:45 -0300

    feat: set axios base url with enviroment variables

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import UserProvider from "./contexts/UserContext";
+import axios from "axios";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
