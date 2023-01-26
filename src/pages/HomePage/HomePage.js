1fe1f064363210ee13cc3d719bddd6d3f475f226
encarnacao <cencarnacaoq@gmail.com>
2023-01-26 11:43:56 -0300

    feat: render components on home screen

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,19 +1,48 @@
-import { HomePageStyle } from "./style";
+import { HomePageStyle, ProductContainer } from "./style";
 import { useContext, useEffect } from "react";
 import { UserContext } from "../../contexts/UserContext";
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
+import Product from "../../components/Product/Product";
 
+const MOCKDATA = [
+	{
+		id: 1,
+		name: "Produto 1",
+		price: 10,
+		image: "https://picsum.photos/200/300",
+	},
+	{
+		id: 2,
+		name: "Produto 2",
+		price: 20,
+		image: "https://picsum.photos/200/300",
+	},
+	{
+		id: 3,
+		name: "Produto 3",
+		price: 30,
+		image: "https://picsum.photos/200/300",
+	},
+];
 
 export default function HomePage() {
-    const { setVisibleHeader } = useContext(UserContext);
-    useEffect(() => {
-        setVisibleHeader(true);
-        // eslint-disable-next-line
-    }, []);
+	const { setVisibleHeader } = useContext(UserContext);
+	useEffect(() => {
+		setVisibleHeader(true);
+		// eslint-disable-next-line
+	}, []);
 	return (
 		<HomePageStyle>
-			<h1>HomePage</h1>
+			<h1>Products</h1>
+			<ProductContainer>
+				{MOCKDATA.map((product) => (
+					<Product
+						name={product.name}
+						price={product.price}
+						image={product.image}
+						key={product.id}
+					/>
+				))}
+			</ProductContainer>
 		</HomePageStyle>
 	);
 }
--- src/pages/HomePage/style.js
+++ src/pages/HomePage/style.js
@@ -3,4 +3,17 @@ import styled from 'styled-components';
 export const HomePageStyle = styled.div`
     margin-top: 70px;
     margin-bottom: 70px;
+    padding: 20px;
+    h1{
+        font-size: 30px;
+        font-weight: 700;
+    }
 `
+
+export const ProductContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-top: 20px;
+    width: 100%;
+`;
