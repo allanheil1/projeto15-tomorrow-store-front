77c320d6044e63fbba488a32c70429ba68f85683
encarnacao <cencarnacaoq@gmail.com>
2023-01-27 17:31:39 -0300

    feat: render cart items on shopping cart page

--- src/pages/CartPage/CartPage.js
+++ src/pages/CartPage/CartPage.js
@@ -1,9 +1,31 @@
-import { } from './style';
+import { useContext } from "react";
+import { OrderContext } from "../../contexts/OrderContext";
+import { CartContainer, CheckoutContainer, HeaderContainer } from "./style";
+import CartItem from "../../components/CartItem/CartItem";
 
 export default function CartPage() {
+	const { cartList } = useContext(OrderContext);
 
-    return(
-        <h1>CartPage</h1>
-    )
-
+	return (
+		<CartContainer>
+			<div>
+                <HeaderContainer>
+                    <h1>Item</h1>
+                    <h1>Price</h1>
+                </HeaderContainer>
+				{cartList.map((item) => (
+					<CartItem key={item._id} item={item} />
+				))}
+			</div>
+			<CheckoutContainer>
+				<div>
+					<p>
+						Subtotal: ${" "}
+						{cartList.reduce((acc, item) => acc + item.price, 0)}
+					</p>
+				</div>
+				<button>Checkout</button>
+			</CheckoutContainer>
+		</CartContainer>
+	);
 }
--- src/pages/CartPage/style.js
+++ src/pages/CartPage/style.js
@@ -1,1 +1,67 @@
 import styled from 'styled-components';
+
+export const CartContainer = styled.div`
+    margin: 70px 0;
+    width: 100vw;
+    height: calc(100vh - 140px);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+    > div{
+        width: 100%;
+    }
+`;
+
+export const CheckoutContainer = styled.div`
+    padding: 20px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 0 0 10px 0 rgba(0,0,0,0.2);
+    > div{
+        width: 100%;
+        display: flex;
+        justify-content: flex-end;
+        margin-bottom: 20px;
+    }
+    > div > p{
+        font-size: 1.2rem;
+        font-weight: 600;
+
+    }
+    > button{
+        width: 100%;
+        padding: 10px;
+        border: none;
+        border-radius: 5px;
+        background: linear-gradient(180deg, #79e499 0%, #0dcdf4 100%);
+        color: white;
+        font-size: 1.2rem;
+        font-weight: 600;
+        cursor: pointer;
+    }  
+`;
+
+export const HeaderContainer = styled.div`
+    width: 100%;
+    height: 50px;
+    display: grid;
+    grid-template-columns: 1fr 2fr 1fr 1fr;
+    grid-template-areas: "image name price remove";
+    align-items: center;
+    border-bottom: 1px solid #e0e0e0;
+    padding: 10px 25px;
+    > h1{
+        font-size: 1.2rem;
+        font-weight: 600;
+        text-align: center;
+    }
+    > h1:first-child{
+        grid-area: image;
+    }
+    > h1:nth-child(2){
+        grid-area: price;
+    }
+`;
