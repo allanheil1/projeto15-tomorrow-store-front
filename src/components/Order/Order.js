b23732f2240e9bd8a5c8683dbfe72f9110d6dc9b
allanheil1 <allanheil1@gmail.com>
2023-01-28 23:57:08 -0300

    layout/feat: OrdersPage/getOrders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,1 +1,24 @@
-import { } from './style';
+import { OrderStyle, HeaderOrder, ItemsOrder } from './style';
+
+export default function Order({orderId, date, paymethod, totalPrice, itemsList}){
+    
+    return(
+        <OrderStyle>
+
+            <HeaderOrder>
+                <h1><span>Order number:</span> {orderId}</h1>
+                <h2><span>Date:</span>  {date}</h2>
+                <h3><span>Price:</span>  ${totalPrice.toFixed(2)}</h3>
+                <h4><span>Payment method:</span>  {paymethod}</h4>
+            </HeaderOrder>
+
+            <ItemsOrder>
+                <h1>Products</h1>
+                {itemsList.map((item, index) => 
+                    <h2 key={index}>➣{item.name}</h2>
+                )}
+            </ItemsOrder>
+
+        </OrderStyle>
+    )
+}
--- src/components/Order/style.js
+++ src/components/Order/style.js
@@ -1,1 +1,33 @@
 import styled from 'styled-components';
+
+export const OrderStyle = styled.div`
+    width: 90%;
+    display: flex;
+    justify-content: space-between;
+    margin: 10px 20px;
+    padding: 10px;
+    border: 1px solid #020e27;
+    border-radius: 5px;
+    h1{
+        font-size: 1.2rem;
+        font-weight: 600;
+        margin-bottom: 5px;
+    }
+`
+
+export const HeaderOrder = styled.div`
+    span{
+        font-weight: 600;
+    }
+    h1 , h2 , h3 , h4 {
+        margin-bottom: 3px;
+    }
+`
+
+export const ItemsOrder = styled.div`
+    width: 145px;
+    h1 , h2{
+        margin-bottom: 3px;
+    }
+
+`
