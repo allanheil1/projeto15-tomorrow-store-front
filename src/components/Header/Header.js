ef37cbae5d165ec4071c9ae8603b5691e7a8a9d8
encarnacao <cencarnacaoq@gmail.com>
2023-01-29 14:33:48 -0300

    feat: add user photo to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,16 @@
-import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { HiOutlineLogout } from 'react-icons/hi';
 
 import { } from './style';
 import Logo from '../../assets/logo.png';
 import { HeaderStyle, LogOutStyle, LogoStyle, PhotoStyle } from './style.js';
-import UserContext from '../../contexts/UserContext';
 
-export default function Header(){
-
-    //const { userPhoto } = useContext(UserContext);
-
+export default function Header({userPhoto}){
     return(
         <HeaderStyle>
 
             <PhotoStyle>
-                <img src={Logo}/>
-                {/* {userPhoto} */}
+                <img src={userPhoto}/>
             </PhotoStyle>
 
             <LogoStyle>
